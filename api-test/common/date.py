d3f63b8bb862519e352df520d65b16357992d860
smiwind <huong.trinh@themartec.com>
2024-03-20 19:36:10 +0700

    remove api studio folder

--- api-test/common/date.py
+++
@@ -1,9 +0,0 @@
-from datetime import date
-
-
-def get_date_as_yyyymmdd():
-    # Get the current date object
-    today = date.today()
-
-    # Format the date object as YYYY-MM-DD
-    return today.strftime("%Y-%m-%d")
